d85ce6c68e84cc499683798cf89ca745d8703b2d
alafin <alafin@ukr.net>
2009-01-19 19:50:01 +0200

    add email notifications

--- apps/forum/subscription.py
+++ apps/forum/subscription.py
@@ -1,11 +1,46 @@
 from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
 from django.core.urlresolvers import reverse
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 
 from apps.forum import settings as forum_settings
+from apps.forum.util import absolute_url
 
-def notify_subscribers(post):
+TOPIC_SUBSCRIPTION_TEXT_TEMPLATE = (u"""New reply from %(username)s to topic that you have subscribed on.
+---
+%(message)s
+---
+See topic: %(post_url)s
+Unsubscribe %(unsubscribe_url)s""")
+
+PM_RECIPIENT_TEXT_TEMPLATE = (u"""User %(username)s have sent your the new private message.
+---
+%(message)s
+---
+See message online: %(pm_url)s""")
+
+
+def send_mail(rec_list, subject, text, html=None):
+    """
+    Shortcut for sending email.
+    """
+
+    from_email = settings.DEFAULT_FROM_EMAIL
+
+    msg = EmailMultiAlternatives(subject, text, from_email, rec_list)
+    if html:
+        msg.attach_alternative(html, "text/html")
+    if forum_settings.EMAIL_DEBUG:
+        print '---begin---'
+        print 'To:', rec_list
+        print 'Subject:', subject
+        print 'Body:', text
+        print '---end---'
+    else:
+        msg.send(fail_silently=True)
+
+
+def notify_topic_subscribers(post):
     from apps.forum.models import Post
 
     topic = post.topic
@@ -13,30 +48,25 @@ def notify_subscribers(post):
         for user in topic.subscribers.all():
             if user != post.user:
                 subject = u'RE: %s' % topic.name
-                from_email = settings.DEFAULT_FROM_EMAIL
                 to_email = user.email
-                text_content = text_version(post)
+                text_content = TOPIC_SUBSCRIPTION_TEXT_TEMPLATE % {
+                        'username': post.user.username,
+                        'message': post.body_text,
+                        'post_url': absolute_url(post.get_absolute_url()),
+                        'unsubscribe_url': absolute_url(reverse('pybb_delete_subscription', args=[post.topic.id])),
+                    }
                 #html_content = html_version(post)
-                msg = EmailMultiAlternatives(subject, text_content, from_email, [to_email])
-                #msg.attach_alternative(html_content, "text/html")
-                msg.send(fail_silently=True)
+                send_mail([to_email], subject, text_content)
 
 
-TEXT_TEMPLATE = _(u"""New reply from %(username)s to topic that you have subscribed on.
----
-%(message)s
----
-See topic: %(post_url)s
-Unsubscribe %(unsubscribe_url)s""")
-
-def text_version(post):
-    data = TEXT_TEMPLATE % {
-        'username': post.user.username,
-        'message': post.body_text,
-        'post_url': absolute_url(post.get_absolute_url()),
-        'unsubscribe_url': absolute_url(reverse('forum_delete_subscription', args=[post.topic.id])),
-    }
-    return data
-
-def absolute_url(uri):
-    return 'http://%s%s' % (forum_settings.FORUM_HOST, uri)
+def notify_pm_recipients(pm):
+    from apps.forum.models import PrivateMessage
+    
+    subject = (u'There are new messages')
+    to_email = pm.dst_user.email
+    text_content = PM_RECIPIENT_TEXT_TEMPLATE % {
+        'username': pm.src_user.username,
+        'message': pm.body_text,
+        'pm_url': absolute_url(pm.get_absolute_url()),
+     }
+    send_mail([to_email], subject, text_content)
