8c27a3a5866d14082a1e72a341509b84f9bb513b
slav0nic <slav0nic0@gmail.com>
2009-01-21 01:13:46 +0200

    replace render_to; code refactoring

--- apps/forum/util.py
+++ apps/forum/util.py
@@ -15,28 +15,41 @@ from django.template.defaultfilters import urlize as django_urlize
 
 from apps.forum import settings as forum_settings
 
-def render_to(template_path):
+def render_to(template):
     """
-    Expect the dict from view. Render returned dict with
-    RequestContext.
-    """
+    Decorator for Django views that sends returned dict to render_to_response function
+    with given template and RequestContext as context instance.
 
-    def decorator(func):
-        def wrapper(request, *args, **kwargs):
-            output = func(request, *args, **kwargs)
-            if not isinstance(output, dict):
-                return output
-            kwargs = {'context_instance': RequestContext(request)}
-            if 'MIME_TYPE' in output:
-                kwargs['mimetype'] = output.pop('MIME_TYPE')
-            if 'TEMPLATE' in output:
-                template = output.pop('TEMPLATE')
-            else:
-                template = template_path
-            return render_to_response(template, output, **kwargs)
+    If view doesn't return dict then decorator simply returns output.
+    Additionally view can return two-tuple, which must contain dict as first
+    element and string with template name as second. This string will
+    override template name, given as parameter
+
+    Parameters:
+
+     - template: template name to use
+
+    Examples::
+
+      @render_to('some/tmpl.html')
+      def view(request):
+          if smth:
+              return {'context': 'dict'}
+          else:
+              return {'context': 'dict'}, 'other/tmpl.html'
+
+     2006-2009 Alexander Solovyov, new BSD License
+    """
+    def renderer(func):
+        def wrapper(request, *args, **kw):
+            output = func(request, *args, **kw)
+            if isinstance(output, (list, tuple)):
+                return render_to_response(output[1], output[0], RequestContext(request))
+            elif isinstance(output, dict):
+                return render_to_response(template, output, RequestContext(request))
+            return output
         return wrapper
-
-    return decorator
+    return renderer
 
 def absolute_url(path):
     return 'http://%s%s' % (forum_settings.HOST, path)
