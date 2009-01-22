8082f0eca832ae9725cd3b23304a893d95c9368e
alafin <alafin@ukr.net>
2009-01-22 20:16:58 +0200

    update highlight.js version to 5.0
    --HG--
    rename : static/forum/css/hljs_styles/phpbb_blue.css => static/forum/css/hljs_styles/djangobb.css

--- static/forum/js/languages/javascript.js
+++ static/forum/js/languages/javascript.js
@@ -1,7 +1,11 @@
+/*
+Language: Javascript
+*/
+
 hljs.LANGUAGES.javascript = {
   defaultMode: {
     lexems: [hljs.UNDERSCORE_IDENT_RE],
-    contains: ['string', 'comment', 'number', 'regexp', 'function'],
+    contains: ['string', 'comment', 'number', 'regexp_container', 'function'],
     keywords: {
       'keyword': {'in': 1, 'if': 1, 'for': 1, 'while': 1, 'finally': 1, 'var': 1, 'new': 1, 'function': 1, 'do': 1, 'return': 1, 'void': 1, 'else': 1, 'break': 1, 'catch': 1, 'instanceof': 1, 'with': 1, 'throw': 1, 'case': 1, 'default': 1, 'try': 1, 'this': 1, 'switch': 1, 'continue': 1, 'typeof': 1, 'delete': 1},
       'literal': {'true': 1, 'false': 1, 'null': 1}
@@ -15,12 +19,20 @@ hljs.LANGUAGES.javascript = {
     hljs.QUOTE_STRING_MODE,
     hljs.BACKSLASH_ESCAPE,
     {
+      className: 'regexp_container',
+      begin: '(' + hljs.RE_STARTERS_RE + '|case|return|throw)\\s*', end: '^', noMarkup: true,
+      lexems: [hljs.IDENT_RE],
+      keywords: {'return': 1, 'throw': 1, 'case': 1},
+      contains: ['comment', 'regexp'],
+      relevance: 0
+    },
+    {
       className: 'regexp',
       begin: '/.*?[^\\\\/]/[gim]*', end: '^'
     },
     {
       className: 'function',
-      begin: 'function\\b', end: '{',
+      begin: '\\bfunction\\b', end: '{',
       lexems: [hljs.UNDERSCORE_IDENT_RE],
       keywords: {'function': 1},
       contains: ['title', 'params']
