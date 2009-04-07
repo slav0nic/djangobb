62cace73880557888ec3e6a735a4e77715e17efa
slav0nic <slav0nic0@gmail.com>
2009-04-07 15:49:12 +0300

    unused import

--- apps/forum/middleware.py
+++ apps/forum/middleware.py
@@ -3,7 +3,6 @@ from datetime import datetime, timedelta
 from django.core.cache import cache
 from django.utils import translation
 from django.conf import settings as global_settings
-from forum import settings
 
 from forum import settings as forum_settings
 
@@ -30,20 +29,23 @@ class ForumMiddleware(object):
 class UsersOnline(object):
     def process_request(self, request):
         now = datetime.now()
-        delta = now - timedelta(minutes=settings.USER_ONLINE_TIMEOUT)
+        delta = now - timedelta(minutes=forum_settings.USER_ONLINE_TIMEOUT)
+        users_online = cache.get('users_online', {})
+        guests_online = cache.get('guests_online', {})
 
         if request.user.is_authenticated():
-            users_online = cache.get('users_online', {})
             users_online[request.user.id] = now
-            for user_id in users_online:
-                if users_online[user_id] < delta:
-                    del users_online[user_id]
-            cache.set('users_online', users_online, 60*60*24)
         else:
-            guests_online = cache.get('guests_online', {})
             guest_sid = request.COOKIES.get(global_settings.SESSION_COOKIE_NAME, '')
             guests_online[guest_sid] = now
-            for guest_id in guests_online:
-                if guests_online[guest_id] < delta:
-                    del guests_online[guest_id]
-            cache.set('guests_online', guests_online, 60*60*24)
+
+        for user_id in users_online:
+            if users_online[user_id] < delta:
+                del users_online[user_id]
+
+        for guest_id in guests_online:
+            if guests_online[guest_id] < delta:
+                del guests_online[guest_id]
+
+        cache.set('users_online', users_online, 60*60*24)
+        cache.set('guests_online', guests_online, 60*60*24)
