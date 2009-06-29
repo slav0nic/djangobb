c69b9ca93649b8d33ec974c56fb3de3e9b2e145e
slav0nic <slav0nic0@gmail.com>
2009-06-29 23:47:45 +0300

    fix feed error

--- apps/forum/feeds.py
+++ apps/forum/feeds.py
@@ -1,4 +1,5 @@
 from django.contrib.syndication.feeds import Feed, FeedDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist
 from django.utils.feedgenerator import Atom1Feed
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
