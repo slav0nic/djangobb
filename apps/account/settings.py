ae0db745fd0339a62f403b7c25f4442222a66570
slav0nic <slav0nic0@gmail.com>
2009-08-04 11:31:13 +0300

    remove account from repo

--- apps/account/settings.py
+++
@@ -1,4 +0,0 @@
-from django.conf import settings
-
-ACCOUNT_DOMAIN = getattr(settings, 'ACCOUNT_DOMAIN', 'fix.your.settings.com')
-ACCOUNT_AUTH_KEY_TIMEOUT = getattr(settings, 'ACCOUNT_AUTH_KEY_TIMEOUT', 60 * 60 * 24)
