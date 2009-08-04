ce34f28696db867f6a03570c6249212a1c45dfe1
slav0nic <slav0nic0@gmail.com>
2009-08-04 19:35:44 +0300

    remove djapian from repo

--- apps/djapian/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-from djapian.tests.query import *
-from djapian.tests.index import *
-from djapian.tests.search import *
-from djapian.tests.common import *
-from djapian.tests.pagination import *
-from djapian.tests.filtering import *
