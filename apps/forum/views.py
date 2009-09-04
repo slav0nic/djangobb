283d856289b5e379a94d20214a0e960876e811ef
slav0nic <slav0nic0@gmail.com>
2009-09-04 12:59:41 +0300

    fix reverse url

--- apps/forum/views.py
+++ apps/forum/views.py
@@ -504,7 +504,7 @@ def user(request, username):
     else:
         topic_count = Topic.objects.filter(user=user).count()
         if user.forum_profile.post_count < forum_settings.POST_USER_SEARCH and not request.user.is_authenticated():
-            return HttpResponseRedirect(reverse('auth_login') + '?next=%s' % request.path)
+            return HttpResponseRedirect(reverse('openauth_login') + '?next=%s' % request.path)
         return {'profile': user,
                 'topic_count': topic_count,
                }
