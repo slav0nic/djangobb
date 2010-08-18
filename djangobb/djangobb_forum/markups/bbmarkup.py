b9d547ac526abeb940be44fa5c601c8a8053c7b0
slav0nic <slav0nic0@gmail.com>
2010-08-18 13:20:52 +0300

    #88: bbmarkup updated

--- djangobb/djangobb_forum/markups/bbmarkup.py
+++ djangobb/djangobb_forum/markups/bbmarkup.py
@@ -7,38 +7,6 @@ from django.utils.safestring import mark_safe
 
 __all__ = ('BBCODE_RULES', 'bbcode')
 
-BBCODE_RULES = [ (r'\[url\](.*?)\[/url\]', r'<a href="\1">\1</a>'),
-        (r'\[url=(.*?)\](.*?)\[/url\]', r'<a href="\1">\2</a>'),
-        (r'\[link\](.*?)\[/link\]', r'<a href="\1">\1</a>'),
-        (r'\[link=(.*?)\](.*?)\[/link\]', r'<a href="\1">\2</a>'),
-        (r'\[email\](.*?)\[/email\]', r'<a href="mailto:\1">\1</a>'),
-        (r'\[email=(.*?)\](.*?)\[/email\]', r'<a href="mailto:\1">\2</a>'),
-        (r'\[img\](.*?)\[/img\]', r'<img src="\1">'),
-        (r'\[img=(.*?)\](.*?)\[/img\]', r'<img src="\1" alt="\2">'),
-        (r'\[color=([a-zA-Z]*|\#?[0-9a-fA-F]{6})\](.*?)\[/color\]', r'<span style="color:\1">\2</span>'),
-        (r'\[b\](.*?)\[/b\]', r'<strong>\1</strong>'),
-        (r'\[i\](.*?)\[/i\]', r'<em>\1</em>'),
-        (r'\[u\](.*?)\[/u\]', r'<u>\1</u>'),
-        (r'\[s\](.*?)\[/s\]', r'<strike>\1</strike>'),
-        (r'\[quote\](.*?)\[/quote\]', r'<blockquote>\1</blockquote>'),
-        (r'\[quote=(.*?)\](.*?)\[/quote\]', r'<blockquote><em>\1</em> <br /> \2</blockquote>'),
-        (r'\[center\](.*?)\[/center\]', r'<div style="text-align: center;">\1</div>'),
-        (r'\[big\](.*?)\[/big\]', r'<big>\1</big>'),
-        (r'\[small\](.*?)\[/small\]', r'<small>\1</small>'),
-        (r'\[list\](.*?)\[/list\]', r'<ul>\1</ul>'),
-        (r'\[list\=(\d+)\](.*?)\[/list\]', r'<ol start="\1">\2</ol>'),
-        (r'\[\*\](.*?)<br./>', r'<li>\1</li>'),
-        (r'\[br\]', r'<br />') ]
-
-BBCODE_RULES += getattr(settings, 'BBMARKUP_EXTRA_RULES', [])
-
-
-BBCODE_RULES_COMPILED = []
-for bbset in (getattr(settings, 'BBMARKUP_CUSTOM_RULES', []) or BBCODE_RULES):
-    p = re.compile(bbset[0], re.DOTALL)
-    BBCODE_RULES_COMPILED.append((p, bbset[1]))
-
-
 def code_parser(matchobj):
     """
     Escaping bbcode and html tags between [code] tags.
@@ -50,6 +18,48 @@ def code_parser(matchobj):
     value = value.replace('<br />', '\n')
     return "<pre><code>%s</code></pre>" % value
 
+"""
+BBcode rule format:
+    'pattern' and 'repl'' - params for re.sub(); 'repl' can be function
+    'sortkey' - used to sort rules from highest to lowest; default value: 0
+    'nested' - show how many time tag can be nested to itself; only for [quote] now
+"""
+
+BBCODE_RULES = [
+        {'pattern': r'\[code\](.*?)\[/code\]', 'repl': code_parser, 'sortkey': 100},
+        {'pattern': r'\[url\](.*?)\[/url\]', 'repl': r'<a href="\1">\1</a>'},
+        {'pattern': r'\[url=(.*?)\](.*?)\[/url\]', 'repl': r'<a href="\1">\2</a>'},
+        {'pattern': r'\[link\](.*?)\[/link\]', 'repl': r'<a href="\1">\1</a>'},
+        {'pattern': r'\[link=(.*?)\](.*?)\[/link\]', 'repl': r'<a href="\1">\2</a>'},
+        {'pattern': r'\[email\](.*?)\[/email\]', 'repl': r'<a href="mailto:\1">\1</a>'},
+        {'pattern': r'\[email=(.*?)\](.*?)\[/email\]', 'repl': r'<a href="mailto:\1">\2</a>'},
+        {'pattern': r'\[img\](.*?)\[/img\]', 'repl': r'<img src="\1">'},
+        {'pattern': r'\[img=(.*?)\](.*?)\[/img\]', 'repl': r'<img src="\1" alt="\2">'},
+        {'pattern': r'\[color=([a-zA-Z]*|\#?[0-9a-fA-F]{6})\](.*?)\[/color\]', 'repl': r'<span style="color:\1">\2</span>'},
+        {'pattern': r'\[b\](.*?)\[/b\]', 'repl': r'<strong>\1</strong>'},
+        {'pattern': r'\[i\](.*?)\[/i\]', 'repl': r'<em>\1</em>'},
+        {'pattern': r'\[u\](.*?)\[/u\]', 'repl': r'<u>\1</u>'},
+        {'pattern': r'\[s\](.*?)\[/s\]', 'repl': r'<strike>\1</strike>'},
+        {'pattern': r'\[quote\](.*?)\[/quote\]', 'repl': r'<blockquote>\1</blockquote>', 'nested': 5},
+        {'pattern': r'\[quote=(.*?)\](.*?)\[/quote\]', 'repl': r'<blockquote><em>\1</em> <br /> \2</blockquote>', 'nested': 5},
+        {'pattern': r'\[center\](.*?)\[/center\]', 'repl': r'<div style="text-align: center;">\1</div>'},
+        {'pattern': r'\[big\](.*?)\[/big\]', 'repl': r'<big>\1</big>'},
+        {'pattern': r'\[small\](.*?)\[/small\]', 'repl': r'<small>\1</small>'},
+        {'pattern': r'\[list\](.*?)\[/list\]', 'repl': r'<ul>\1</ul>'},
+        {'pattern': r'\[list\=(\d+)\](.*?)\[/list\]', 'repl': r'<ol start="\1">\2</ol>'},
+        {'pattern': r'\[\*\](.*?)<br./>', 'repl': r'<li>\1</li>'},
+        {'pattern': r'\[br\]', 'repl': r'<br />'},
+]
+
+BBCODE_RULES += getattr(settings, 'BBMARKUP_EXTRA_RULES', [])
+BBCODE_RULES.sort(key=lambda r: r.get('sortkey', 0), reverse=True)
+
+BBCODE_RULES_COMPILED = []
+for bbset in (getattr(settings, 'BBMARKUP_CUSTOM_RULES', []) or BBCODE_RULES):
+    bbset['pattern'] = re.compile(bbset['pattern'], re.DOTALL)
+    bbset.setdefault('sortkey', 0)
+    bbset.setdefault('nested', 0)
+    BBCODE_RULES_COMPILED.append(bbset)
 
 def bbcode(value, code_parser=code_parser):
     """
@@ -58,27 +68,29 @@ def bbcode(value, code_parser=code_parser):
     u'<pre><code>print &quot;Lorem &#91;b&#93;imsum&#91;b&#93;&quot;</code></pre>'
     >>> bbcode('[i]Lorem[/i] \\n [s]imsum[/s]')
     u'<em>Lorem</em> <br /> <strike>imsum</strike>'
-    >>> bbmarkup.bbcode('[list] [*] 1\n [*]2\n [*] 3\n[/list]')
-    u'<ul> <li>1</li> <li>2</li> <li>3</li></ul>'
-    >>> bbmarkup.bbcode('[list=2] [*] a\n [*]b\n [*] c\n[/list]')
-    u'<ol start="2"> <li>a</li> <li>b</li> <li>c</li></ol>'
-    >>> bbmarkup.bbcode("[code]print 123\nprint '<br/>'[/code]")
-    u'<pre><code>print 123\nprint &#39;&lt;br/&gt;&#39;</code></pre>'
-    >>> bbmarkup.bbcode('[quote=test user]Test quote text[/quote]')
+    >>> bbcode('[list] [*] 1\\n [*]2\\n [*] 3\\n[/list]')
+    u'<ul> <li> 1</li> <li>2</li> <li> 3</li></ul>'
+    >>> bbcode('[list=2] [*] a\\n [*]b\\n [*] c\\n[/list]')
+    u'<ol start="2"> <li> a</li> <li>b</li> <li> c</li></ol>'
+    >>> bbcode("[code]print 123\\nprint '<br/>'[/code]")
+    u'<pre><code>print 123\\nprint &#39;&lt;br/&gt;&#39;</code></pre>'
+    >>> bbcode('[quote=test user]Test quote text[/quote]')
     u'<blockquote><em>test user</em> <br /> Test quote text</blockquote>'
-    >>> bbmarkup.bbcode('[color=red]Lorem[/color]')
+    >>> bbcode("[quote]Lorem [quote=sl]imsum[/quote] blabla [/quote]")
+    u'<blockquote>Lorem <blockquote><em>sl</em> <br /> imsum</blockquote> blabla </blockquote>'
+    >>> bbcode('[color=red]Lorem[/color]')
     u'<span style="color:red">Lorem</span>'
-    >>> bbmarkup.bbcode('[color=#FAaF12]Lorem[/color]')
+    >>> bbcode('[color=#FAaF12]Lorem[/color]')
     u'<span style="color:#FAaF12">Lorem</span>'
-    >>> bbmarkup.bbcode('[color=#FAaF121]Lorem[/color]')
+    >>> bbcode('[color=#FAaF121]Lorem[/color]')
     u'[color=#FAaF121]Lorem[/color]'
     
     """
 
     value = escape(value)
     value = linebreaksbr(value)
-    value = re.sub(re.compile(r'\[code\](.*?)\[/code\]', re.DOTALL), code_parser, value)
     for bbset in BBCODE_RULES_COMPILED:
-        value = bbset[0].sub(bbset[1], value)
+        for _ in xrange(bbset['nested'] + 1):
+            value = bbset['pattern'].sub(bbset['repl'], value)
 
     return mark_safe(value)
