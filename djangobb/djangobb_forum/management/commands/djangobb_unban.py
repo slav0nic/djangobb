ea5fdf3c6ee19b86bb7aea55570fcf1fe0193895
slav0nic <slav0nic0@gmail.com>
2010-11-06 17:33:44 +0200

    added crontab example; unban command renamed to djangobb_unban
    --HG--
    rename : djangobb/djangobb_forum/management/commands/unban.py => djangobb/djangobb_forum/management/commands/djangobb_unban.py

--- djangobb/djangobb_forum/management/commands/djangobb_unban.py
+++ djangobb/djangobb_forum/management/commands/djangobb_unban.py
@@ -11,18 +11,18 @@ class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option('--all', action='store_true', dest='all', default=False, 
                     help=u'Unban all users'),
-        make_option('--in-time', action='store_true', dest='in-time', default=False, 
-                    help=u'Unban users in time'),
+        make_option('--by-time', action='store_true', dest='by-time', default=False, 
+                    help=u'Unban users by time'),
     )
     help = u'Unban users'
-    
+
     def handle(self, *args, **options):
         if options['all']:
             bans = Ban.objects.all()
             user_ids = bans.values_list('user', flat=True)
             users = User.objects.filter(id__in=user_ids).update(is_active=True)
             bans.delete()
-        elif options['in-time']:
+        elif options['by-time']:
             bans = Ban.objects.filter(ban_end__lte=datetime.now())
             user_ids = bans.values_list('user', flat=True)
             users = User.objects.filter(id__in=user_ids).update(is_active=True)
