076094626b51b11d39ee8e6ee897076d7ec9283b
slav0nic <slav0nic0@gmail.com>
2011-05-17 15:02:33 +0300

    added file

---
+++ djangobb/djangobb_forum/tests/test_templatetags.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from djangobb_forum.models import Post
+from djangobb_forum.templatetags.forum_extras import profile_link, link, lofi_link\
+    attachment_link
+
+
+class TestLinkTags(TestCase):
+    fixtures = ['test_forum.json']
+
+    def setUp(self):
+        self.user = User.objects.get(pk=1)
+        self.post = Post.objects.get(pk=1)
+
+    def test_profile_link(self):
+        plink = profile_link(self.user)
+        self.assertEqual(plink, u"<a href=\"/forum/user/djangobb/\">djangobb</a>")
+    
+    def test_link(self):
+        l = link(self.post)
+        self.assertEqual(l, "<a href=\"/forum/post/1/\">Test Body</a>")
+
+    def test_lofi_link(self):
+        l = lofi_link(self.post)
+        self.assertEqual(l, "<a href=\"/forum/post/1/lofi/\">Test Body</a>")
+    
