015193b964a9128718ed5c22aa2021f8802be7a0
slav0nic <slav0nic0@gmail.com>
2011-05-17 15:02:22 +0300

    stupid test link tests

--- djangobb/djangobb_forum/tests/__init__.py
+++ djangobb/djangobb_forum/tests/__init__.py
@@ -2,3 +2,4 @@ from test_forum import TestForum
 from test_reputation import TestReputation
 from test_profile import TestProfile
 from test_utils import TestParsers, TestPaginators
+from test_templatetags import TestTags
