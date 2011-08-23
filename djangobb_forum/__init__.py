71bdef721d098236bd6f0b3eb0c71e27c7fc8f25
slav0nic <slav0nic0@gmail.com>
2011-08-23 10:27:40 +0300

    fix comment

--- djangobb_forum/__init__.py
+++ djangobb_forum/__init__.py
@@ -2,7 +2,7 @@ __all__ = ['__version__', 'get_version']
 
 version_info = (0, 0, 1, 'a', 0)
 # format: ('major', 'minor', 'micro', 'releaselevel', 'serial')
-# __version__ format compatible with ditutils2.version.NormalizedVersion
+# __version__ format compatible with distutils2.version.NormalizedVersion
 
 
 def get_version():
