3c536e4217f47dd98647f26abca5d09d0845fa71
Igor Yanchenko <yanchenko.igor@gmail.com>
2012-03-19 11:35:33 +0200

    lofi fix errors

--- djangobb_forum/templatetags/forum_extras.py
+++ djangobb_forum/templatetags/forum_extras.py
@@ -11,7 +11,7 @@ from django.utils.html import escape
 from django.utils.hashcompat import md5_constructor
 from django.contrib.humanize.templatetags.humanize import naturalday
 
-from pagination.templatetags.pagination_tags import paginate, DEFAULT_WINDOW
+from pagination.templatetags.pagination_tags import paginate
 
 from djangobb_forum.models import Report
 from djangobb_forum import settings as forum_settings
@@ -97,8 +97,8 @@ def pagination(context, adjacent_pages=1):
 
 
 @register.inclusion_tag('djangobb_forum/lofi/pagination.html',takes_context=True)
-def lofi_pagination(context, window=DEFAULT_WINDOW, hashtag=''):
-    return paginate(context, window, hashtag)
+def lofi_pagination(context):
+    return paginate(context)
 
 @register.simple_tag
 def link(object, anchor=u''):
