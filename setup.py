79f4f9712432258b92b9f1c2e30d9a0fd8d0bf02
slav0nic <slav0nic0@gmail.com>
2013-03-19 14:30:27 +0200

    Remove binary mo files from repo. Compilemessages will be start automatically at package install/build egg via setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,40 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
+from setuptools.command.install_lib import install_lib as _install_lib
+from distutils.command.build import build as _build
+from distutils.cmd import Command
 from djangobb_forum import get_version
+
+
+class compile_translations(Command):
+    description = 'compile message catalogs to MO files via django compilemessages'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import os
+        import sys
+        from django.core.management.commands.compilemessages import \
+            compile_messages
+        curdir = os.getcwd()
+        os.chdir(os.path.realpath('djangobb_forum'))
+        compile_messages(stderr=sys.stderr)
+        os.chdir(curdir)
+
+
+class build(_build):
+    sub_commands = [('compile_translations', None)] + _build.sub_commands
+
+
+class install_lib(_install_lib):
+    def run(self):
+        self.run_command('compile_translations')
+        _install_lib.run(self)
 
 setup(name='djangobb_forum',
     version=get_version(),
@@ -12,13 +46,15 @@ setup(name='djangobb_forum',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-            'django>=1.3.1',
+            'django>=1.4.5',
             'pil>=1.1.7',
-            'django-haystack',
+            'django-haystack==1.2.7',
             'django-pagination',
             'south',
             'postmarkup',
             'setuptools'
             ],
-    keywords="django forum bb",
+    keywords='django forum bb',
+    cmdclass={'build': build, 'install_lib': install_lib,
+        'compile_translations': compile_translations}
 )
