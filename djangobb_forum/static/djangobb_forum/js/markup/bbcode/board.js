35859d652f292ae4fb8ce60d0495818c4c29d2f1
Nathan Dinsmore <queryselector@gmail.com>
2013-05-11 17:23:02 -0400

    Fixed #908
    Usernames in posts are now profile links

--- djangobb_forum/static/djangobb_forum/js/markup/bbcode/board.js
+++ djangobb_forum/static/djangobb_forum/js/markup/bbcode/board.js
@@ -10,7 +10,7 @@ function log() {
 }
 //log("board.js loaded");
 
-function copy_paste(id) {   
+function copy_paste(id) {
     var post = $('#' + id);
     var username = post.find(".username").text();
     $.ajax('/discuss/post/' + id.substr(1) + '/source/').done(function (data) {
@@ -27,34 +27,28 @@ function paste(txt) {
 
 function insertAtCaret (textObj, textFieldValue) {
     // log("insertAtCaret(" + textObj + "," + textFieldValue + ")");
-	if (document.all) { 
-		if (textObj.createTextRange && textObj.caretPos && !window.opera) { 
-			var caretPos = textObj.caretPos; 
-			caretPos.text = caretPos.text.charAt(caretPos.text.length - 1) == ' ' ?textFieldValue + ' ' : textFieldValue; 
-		} else { 
-			textObj.value += textFieldValue; 
-		} 
-	} else { 
-		if (textObj.selectionStart) { 
-			var rangeStart = textObj.selectionStart; 
-			var rangeEnd = textObj.selectionEnd; 
-			var tempStr1 = textObj.value.substring(0, rangeStart); 
-			var tempStr2 = textObj.value.substring(rangeEnd, textObj.value.length); 
-			textObj.value = tempStr1 + textFieldValue + tempStr2; 
+	if (document.all) {
+		if (textObj.createTextRange && textObj.caretPos && !window.opera) {
+			var caretPos = textObj.caretPos;
+			caretPos.text = caretPos.text.charAt(caretPos.text.length - 1) == ' ' ?textFieldValue + ' ' : textFieldValue;
+		} else {
+			textObj.value += textFieldValue;
+		}
+	} else {
+		if (textObj.selectionStart) {
+			var rangeStart = textObj.selectionStart;
+			var rangeEnd = textObj.selectionEnd;
+			var tempStr1 = textObj.value.substring(0, rangeStart);
+			var tempStr2 = textObj.value.substring(rangeEnd, textObj.value.length);
+			textObj.value = tempStr1 + textFieldValue + tempStr2;
 			textObj.selectionStart = textObj.selectionEnd = rangeStart + textFieldValue.length;
-		} else { 
-			textObj.value += textFieldValue; 
-		} 
-	} 
+		} else {
+			textObj.value += textFieldValue;
+		}
+	}
 }
 
 $(document).ready(function() {
-    $(".username").click(function() {
-        var nick = $(this).text();
-        paste("[b]"+nick+"[/b]\n");
-    });
-    $(".username").attr('title', 'Click to paste nick name in reply form.');
-    
     window.onbeforeunload = function() {
         var obj = $("textarea#id_body");
         if (obj.length != 1) {
@@ -74,7 +68,7 @@ $(document).ready(function() {
         //log("unbind onbeforeunload");
         window.onbeforeunload = null;
     });
-    
+
     !function () {
         var submitted = false;
         $('#post').submit(function (e) {
