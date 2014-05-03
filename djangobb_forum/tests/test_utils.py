98749b4fb09f69b5f29331dd4d80bc3abd0febd7
Stefan Hajnoczi <stefanha@gmail.com>
2014-05-03 12:22:22 +0300

    Add test case for utils.add_rel_nofollow() Both convert_text_to_html() and add_rel_nofollow() should add the rel="nofollow" attribute to <a> tags.
    Signed-off-by: Stefan Hajnoczi <stefanha@gmail.com>

--- djangobb_forum/tests/test_utils.py
+++ djangobb_forum/tests/test_utils.py
@@ -3,15 +3,16 @@ from django.test import TestCase, RequestFactory
 from django.conf import settings
 
 from djangobb_forum.models import Post
-from djangobb_forum.util import urlize, smiles, convert_text_to_html, paginate
+from djangobb_forum.util import urlize, smiles, add_rel_nofollow, convert_text_to_html, paginate
 
 
 class TestParsers(TestCase):
     def setUp(self):
         self.data_url = "Lorem ipsum dolor sit amet, consectetur http://djangobb.org/ adipiscing elit."
         self.data_smiles = "Lorem ipsum dolor :| sit amet :) <a href=\"http://djangobb.org/\">http://djangobb.org/</a>"
+        self.data_nofollow = "&lt;a&gt;Lorem ipsum&lt;/a&gt; <a href=\"http://djangobb.org/\">http://djangobb.org/</a>"
         self.markdown = ""
-        self.bbcode = "[b]Lorem[/b] [code]ipsum :)[/code] =)"
+        self.bbcode = "[b]Lorem[/b] [code]ipsum :)[/code] [url=http://djangobb.org/]http://djangobb.org/[/url] =)"
 
     def test_urlize(self):
         urlized_data = urlize(self.data_url)
@@ -21,9 +22,13 @@ class TestParsers(TestCase):
         smiled_data = smiles(self.data_smiles)
         self.assertEqual(smiled_data, u"Lorem ipsum dolor <img src=\"{0}djangobb_forum/img/smilies/neutral.png\" /> sit amet <img src=\"{0}djangobb_forum/img/smilies/smile.png\" /> <a href=\"http://djangobb.org/\">http://djangobb.org/</a>".format(settings.STATIC_URL))
 
+    def test_nofollow(self):
+        nofollow_data = add_rel_nofollow(self.data_nofollow)
+        self.assertEqual(nofollow_data, "&lt;a&gt;Lorem ipsum&lt;/a&gt; <a href=\"http://djangobb.org/\" rel=\"nofollow\">http://djangobb.org/</a>")
+
     def test_convert_text_to_html(self):
         bb_data = convert_text_to_html(self.bbcode, 'bbcode')
-        self.assertEqual(bb_data, "<strong>Lorem</strong> <div class=\"code\"><pre>ipsum :)</pre></div>=)")
+        self.assertEqual(bb_data, "<strong>Lorem</strong> <div class=\"code\"><pre>ipsum :)</pre></div><a href=\"http://djangobb.org/\" rel=\"nofollow\">http://djangobb.org/</a> =)")
 
 class TestPaginators(TestCase):
     fixtures = ['test_forum.json']
