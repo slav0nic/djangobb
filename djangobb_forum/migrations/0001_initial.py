826813a4c6b3e9cd8ee7400e8c79eee34a9528a9
slav0nic <slav0nic0@gmail.com>
2015-05-24 17:16:22 +0300

    fix migration for django 1.7

--- djangobb_forum/migrations/0001_initial.py
+++ djangobb_forum/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('auth', '0006_require_contenttypes_0002'),
+        ('auth', '0001_initial'),
     ]
 
     operations = [
